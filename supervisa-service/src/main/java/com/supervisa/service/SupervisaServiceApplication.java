cf661642069ce4364941e4caedd91884819c60f9
Sebastian Murillo <sebasty06@outlook.com>
2025-06-05 20:44:21 -0500

    Agregados los dos proyectos a trabajar, para backend (Springboot) y para frontend (React)

---
+++ supervisa-service/src/main/java/com/supervisa/service/SupervisaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.supervisa.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupervisaServiceApplication.class, args);
+    }
+
+}
---
+++ supervisa-service/src/test/java/com/supervisa/service/SupervisaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supervisa.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
